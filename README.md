b7783a94b6f179af3076a0480950b4ffc18a5f35
Aftab <mohammad.aftab@spglobal.com>
2018-07-21 11:34:15 +0100

    Lists Volumes and Snapshots

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # aws-health-check
 Project to perform checks on an AWS environment
 
+Create a aws profile with access to the resources that you intend to monitor
+
+`aws configure --profile=awschecker`
+
 Install pipenv
 `pip install pipenv`
 
 To run
-`pipenv run python awschecks/awschecks.py`
+`pipenv run python awschecks/awschecks.py <resource> <command> <project>`
+
+where
+<resource> is instances, volumes or snapshots
+<command> is dependant on resource - list, start, stop
+<project> is optional - Project name tag to filter resources based on Project
--- awschecks/awschecks.py
+++ awschecks/awschecks.py
@@ -5,8 +5,54 @@ session = boto3.Session(profile_name='awschecker')
 ec2 = session.resource('ec2')
 
 @click.group()
+def cli():
+	"Command Line Interface"
+
+@cli.group('snapshots')
+def snapshots():
+	"Commands for snapshots"
+@snapshots.command('list')
+@click.option('--project', default=None)
+def list_snapshots(project):
+	"List all snapshots"
+	if project:
+		filters = [{'Name':'tag:Project','Values':[project]}]
+		instances = ec2.instances.filter(Filters=filters)
+	else:
+		instances = ec2.instances.all()
+
+	for instance in instances:
+		for volume in instance.volumes.all():
+			for snapshot in volume.snapshots.all():
+		#tags = { t['Key']:t['Value'] for t in instance.tags or [] }
+				print(', '.join((snapshot.id,snapshot.volume_id,instance.id,snapshot.progress,snapshot.state,snapshot.encrypted and 'Encrypted' or 'Not Encrypted')))
+
+	return
+
+
+@cli.group('volumes')
+def volumes():
+	"Commands for volumes"
+@volumes.command('list')
+@click.option('--project', default=None)
+def list_volumes(project):
+	"List all volumes"
+	if project:
+		filters = [{'Name':'tag:Project','Values':[project]}]
+		instances = ec2.instances.filter(Filters=filters)
+	else:
+		instances = ec2.instances.all()
+
+	for instance in instances:
+		for volume in instance.volumes.all():
+		#tags = { t['Key']:t['Value'] for t in instance.tags or [] }
+			print(', '.join((volume.id,instance.id,volume.state,str(volume.size) + ' GiB',volume.encrypted and 'Encrypted' or 'Not Encrypted')))
+
+	return
+
+@cli.group('instances')
 def instances():
-	"commands to list instances"
+		"""Commands to list instances"""
 
 @instances.command('list')
 @click.option('--project', default=None)
@@ -36,6 +82,23 @@ def stop_instances(project):
 	for i in instances:
 		print("Stopping {0}...".format(i.id))
 		i.stop()
-	return		
+	return
+
+@instances.command('start')
+@click.option('--project',default=None)
+
+def start_instances(project):
+	"Start EC2 instances"
+	if project:
+		filters = [{'Name':'tag:Project','Values':[project]}]
+		instances = ec2.instances.filter(Filters=filters)
+	else:
+		instances = ec2.instances.all()
+
+	for i in instances:
+		print("Starting {0}...".format(i.id))
+		i.start()
+	return
+
 if __name__ == '__main__':
-	instances()
+	 cli()
